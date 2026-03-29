065e8bcd641ba6168eebf7e14e3221d514adb8fa
phynicsmusic <thawzin19992099@gmail.com>
2026-03-29 09:12:28 +0630

    Update userbot.py

--- ChampuMusic/core/userbot.py
+++ ChampuMusic/core/userbot.py
@@ -63,11 +63,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TheShivanshu")
-                await self.one.join_chat("TheChampuClub")
-                await self.one.join_chat("akaChampu")
-                await self.one.join_chat("TheChampu")
-
+                await self.one.join_chat("myanmarbot_music")
+                
             except:
                 pass
             assistants.append(1)
@@ -93,10 +90,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TheShivanshu")
-                await self.two.join_chat("TheChampuClub")
-                await self.two.join_chat("akaChampu")
-                await self.two.join_chat("TheChampu")
+                await self.two.join_chat("myanmarbot_music")
+                
             except:
                 pass
             assistants.append(2)
@@ -117,10 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TheShivanshu")
-                await self.three.join_chat("TheChampuClub")
-                await self.three.join_chat("akaChampu")
-                await self.three.join_chat("TheChampu")
+                await self.three.join_chat("myanmarbot_music")
+                
             except:
                 pass
             assistants.append(3)
@@ -142,10 +135,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TheShivanshu")
-                await self.four.join_chat("TheChampuClub")
-                await self.four.join_chat("akaChampu")
-                await self.four.join_chat("TheChampu")
+                await self.four.join_chat("myanmarbot_music")
+                
             except:
                 pass
             assistants.append(4)
@@ -167,10 +158,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TheShivanshu")
-                await self.five.join_chat("TheChampuClub")
-                await self.five.join_chat("akaChampu")
-                await self.five.join_chat("TheChampu")
+                await self.five.join_chat("myanmarbot_music")
             except:
                 pass
             assistants.append(5)
